c25bcaec99db5c5ebf2be54f44e5945aae51f84b
navarrovitor <vitor.san.nav.nexus@gmail.com>
2022-10-14 17:45:50 -0300

    start to create home text design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 're-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   title = 'home';
